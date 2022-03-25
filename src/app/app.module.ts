8b00ea6be3d72c421ff4390c7fce0fd0d2d54504
BARKAOUI <meriam.barkaoui@etu.univ-grenoble-alpes.fr>
2022-03-25 09:32:51 +0100

    push

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: 'tdlInitial', useValue: {label: 'L3 MIAGE', items: [] }}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
